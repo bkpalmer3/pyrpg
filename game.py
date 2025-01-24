92bd011dcaff7d5eda7f8fffa497e951badeab84
Benjamin Palmer <144744973+bkpalmer3@users.noreply.github.com>
2025-01-24 12:13:42 -0700

    Update game.py
    Create a Hero class in the main function

--- game.py
+++ game.py
@@ -1,2 +1,15 @@
-print("Hello World")
-# I'M BACK BABY
+class Hero ():
+    def __init__(self, name, hp, gold):
+        self.name = name
+        self.hp = hp
+        self.gold = gold
+
+    def Who_Hero(self):
+        print(self.name, self.hp, self.gold)
+
+def main():
+    main_hero = Hero("Tallun", 20, 100)
+    main_hero.Who_Hero()
+
+if __name__ == "__main__":
+    main()
